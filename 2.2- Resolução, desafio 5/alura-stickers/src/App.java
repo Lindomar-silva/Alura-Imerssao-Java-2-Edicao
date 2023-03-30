aed8bd7bc9764b1e9789be22a9e23d15c2bedadf
Lindomar-silva <lindomar.s.silva@gmail.com>
2023-03-30 11:27:18 -0300

    2.2- Resolução, desafio 5

---
+++ 2.2- Resolução, desafio 5/alura-stickers/src/App.java
@@ -0,0 +1,69 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.net.URI;
+import java.net.URL;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
+import java.util.List;
+import java.util.Map;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        // Fazer uma conexão HTTP e buscar os top 250 filmes
+
+        // String imdbKey = System.getenv("VARIAVEL_DE_AMBIENTE");
+        // String url = "https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/TopMovies.json";
+        String url = "https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/MostPopularMovies.json";
+        URI endereco = URI.create(url);
+        var client = HttpClient.newHttpClient();
+        var request = HttpRequest.newBuilder(endereco).GET().build();
+        HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
+        String body =  response.body();
+
+        // Extrair só os dados (titulo, poster, classificação) que interessam no IMDB-API 
+        JsonParser parser = new JsonParser();
+        List<Map<String, String>> listaDeFilmes = parser.parse(body);
+
+        //Exibir e manipular os dados
+        var gerafiguras = new GeradorDeFigurinhas();
+        for (int i = 0; i < 3; i++) {
+            Map<String, String> filme = listaDeFilmes.get(i);
+
+            String urlImagem = filme.get("image");
+            String titulo = filme.get("title");
+
+            InputStream inputStream = new URL(urlImagem).openStream();
+            String nomeArquivo = titulo + ".png";
+
+            double doubleRating = Double.parseDouble(filme.get("imDbRating"));
+
+            String texto;
+            InputStream imagem;
+            
+            if (doubleRating > 7.5) {
+                texto = "TOPZERA";
+                imagem = new FileInputStream(new File("sobreposicao/topzera.jpeg"));
+            } else {
+                texto = "MEDIANO";
+                imagem = new FileInputStream(new File("sobreposicao/mediano.jpg"));
+            }
+
+            gerafiguras.gerar(inputStream, nomeArquivo, texto, imagem);
+
+            System.out.println("\u001b[1mTítulo:\u001b[0m " + filme.get("title"));
+            System.out.println("\u001b[1mPoster\u001b[0m : " + filme.get("image"));
+            System.out.println("\u001b[1mClassificação:\u001b[0m " + filme.get("imDbRating"));
+
+            int estrelas = (int) doubleRating;
+
+            for (int n = 1; n <= estrelas; n++) {
+                System.out.print("⭐");
+            }
+
+            System.out.println();
+        }
+    }
+}
